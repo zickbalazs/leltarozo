ff5653f3df49653c228f5dafbc30e3d2e30caea4
zickbalazs <zickbalazs@turr.hu>
2023-02-03 14:14:31 +0100

    jézus

--- src/components/DashBoard.vue
+++ src/components/DashBoard.vue
@@ -4,9 +4,11 @@
         <div class="col-lg-10 col-11">
             <h2 class="mt-3">Üdv!</h2>
             <div class="row">
-                <DashCard v-for="card in DashCardItems" :card-data="card" />
+                <DashCard v-for="card in DashCardItems" :card-data="card.data" :modal-i-d="card.modalID" />
             </div>
         </div>
+        <Inventory/>
+        <Add/>
     </main>
 </template>
 <style>
@@ -14,19 +16,25 @@
 </style>
 <script lang="ts">
 import NavBar from '@/views/NavBar.vue';
+import Inventory from '@/components/Inventory.vue';
 import DashCard from '@/components/DashCard.vue';
+import Add from '@/views/Add.vue';
 import { DashCardItem } from '@/classes/DashCardItem';
 export default {
     data() {
         return {
             DashCardItems: [
-                new DashCardItem("Új tétel felvétele", "plus-lg", ["#00A62C", "#005416"]),
-                new DashCardItem("Tételek megtekintése", "eye", ["#85E0D4", "#C7AABE"]),
-                new DashCardItem("Tételek szerkesztése", "wrench", ["#FFBF7A", "#8C894C"]),
-                new DashCardItem("Tételek törlése", "trash2", ["#C11E38", "#220B34"]),
+                {
+                    data: new DashCardItem("Új tétel felvétele", "plus-lg", ["#00A62C", "#005416"]),
+                    modalID: "addModal"
+                },
+                {
+                    data: new DashCardItem("Tételek megtekintése", "eye", ["#85E0D4", "#C7AABE"]),
+                    modalID: "inventoryModal"
+                },
             ],
         }
     },
-    components: { NavBar, DashCard }
+    components: { NavBar, DashCard, Inventory, Add}
 }
 </script>
