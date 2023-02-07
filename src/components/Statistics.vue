d723a4e496617065d1b3d76de6cc12942eba5ea6
zickbalazs <zickbalazs@turr.hu>
2023-02-07 14:14:17 +0100

    Rossz a chart

--- src/components/Statistics.vue
+++ src/components/Statistics.vue
@@ -1,7 +1,6 @@
 <template>
     <NavBar />
     <main>
-        <Chart />
     </main>
 </template>
 <style>
@@ -9,16 +8,30 @@
 <script lang="ts">
     import NavBar from '@/views/NavBar.vue';
     import Chart from '@/views/Chart.vue';
+    import { db } from '@/services/dbService';
+    import type { AxiosResponse } from 'axios';
     export default{
         data(){
             return{
-                data:{
-                    asd:"asd"
+                datas:{
+                    name:"Date - Price",
+                    ID: "datepriceChart",
+                    labels:[] as Array<string>,
+                    type:"bar",
+                    datasets:[{
+                        data:[] as Array<number>,
+                        label:"price"
+                    }]
                 }
             }
         },
+        created(){
+            
+        },
         methods: {
-
+            GetDataForChart():AxiosResponse<Array<number>>{
+                return db.GetAll('http://localhost/2-14-SZFT')
+            }
         },
         components:{
             NavBar,
